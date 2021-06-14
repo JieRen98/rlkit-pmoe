9a137e273e825be2781646ab154153214df82cb7
Your Name <you@example.com>
2021-06-14 14:24:59 +0800

    fit for test

--- scripts/run_policy.py
+++ scripts/run_policy.py
@@ -30,11 +30,11 @@ def simulate_policy(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('file', type=str,
+    parser.add_argument('--file', type=str, default="/home/renjie/PycharmProjects/rlkit-pmoe/data/PendulumEnv/PendulumEnv_2021_06_14_14_02_27_0000--s-0/params.pkl",
                         help='path to the snapshot file')
     parser.add_argument('--H', type=int, default=300,
                         help='Max length of rollout')
-    parser.add_argument('--gpu', action='store_true')
+    parser.add_argument('--gpu', default=True, action='store_true')
     args = parser.parse_args()
 
     simulate_policy(args)
